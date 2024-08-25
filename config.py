0fb3a140aaf8ef643d7b51ed8aa197765efcb114
Blasty <79897422+BlastyTheDev@users.noreply.github.com>
2024-08-25 14:56:10 +1000

    Add math for Glicko2, Add configurable fields to a .properties file

---
+++ config.py
@@ -0,0 +1,47 @@
+import os
+
+FILE_NAME: str = 'glicko2.properties'
+
+SYSTEM_CONSTANT: float = 0.5
+UNRANKED_GLICKO: float = 1500
+UNRANKED_RD: float = 350
+UNRANKED_VOLATILITY: float = 0.06
+MAX_GLIXARE_RATING: float = 25000
+
+def set_property(key: str, value: float):
+    match key:
+        case 'system-constant':
+            SYSTEM_CONSTANT = value
+        case 'unranked-glicko':
+            UNRANKED_GLICKO = value
+        case 'unranked-rd':
+            UNRANKED_RD = value
+        case 'unranked-volatility':
+            UNRANKED_VOLATILITY = value
+        case 'max-glixare-rating':
+            MAX_GLIXARE_RATING = value
+
+def load():
+    if os.path.exists(FILE_NAME):
+        with open(FILE_NAME) as f:
+            for line in f.readlines():
+                kv = line.split('=')
+
+                try:
+                    set_property(kv[0], float(kv[1]))
+                except TypeError:
+                    continue
+    else:
+        with open(FILE_NAME, 'w') as f:
+            properties: list[str] = [
+                'system-constant=' + str(SYSTEM_CONSTANT),
+                'unranked-glicko=' + str(UNRANKED_GLICKO),
+                'unranked-rd=' + str(UNRANKED_RD),
+                'unranked-volatility=' + str(UNRANKED_VOLATILITY),
+                'max-glixare-rating=' + str(MAX_GLIXARE_RATING)
+            ]
+
+            for property in properties:
+                f.write(property + '\n')
+
+            f.close()
---
+++ glicko2.py
@@ -0,0 +1,78 @@
+import math
+import numpy
+
+SYSTEM_CONSTANT: float = 0.5
+
+def to_glicko2(glicko: float) -> float:
+    return (glicko - 1500) / 173.7178
+
+def to_rd2(rd: float) -> float:
+    return rd / 173.7178
+
+def to_glicko(glicko2: float) -> float:
+    return 173.7178 * glicko2 + 1500
+
+def to_rd(rd2: float) -> float:
+    return 173.7178 * rd2
+
+def g(rd: float) -> float:
+    return 1 / math.sqrt(1 + 3 * (rd ** 2) / (math.pi ** 2))
+
+def E(glicko2: float, glicko2j: float, rdj: float) -> float:
+    return 1 / (1 + math.exp(-g(rdj) * (glicko2 - glicko2j)))
+
+def variance(glicko2: float, enemy_glicko2: float, enemy_rd2: float) -> float:
+    result_E: float = E(glicko2, enemy_glicko2, enemy_rd2)
+    return ((g(enemy_rd2) ** 2) * result_E * (1 - result_E)) ** -1
+
+def improvement(variance: float, glicko2: float, enemy_glicko2: float, enemy_rd2: float, result: float) -> float:
+    return (g(enemy_rd2) * (result - E(glicko2, enemy_glicko2, enemy_rd2))) * variance
+
+def f(x: float, variance: float, improvement: float, rd2: float, a: float) -> float:
+    result_exp: float = math.exp(x)
+    return (result_exp * ((improvement ** 2) - (rd2 ** 2) - variance - result_exp)
+            / 2 * (((rd2 ** 2) + variance + result_exp) ** 2) - ((x - a) / (SYSTEM_CONSTANT ** 2)))
+
+def new_volatility(volatility: float, variance: float, improvement: float, rd2: float) -> float:
+    a: float = numpy.log(volatility ** 2)
+    b: float
+    c: float
+    k: float
+
+    if improvement ** 2 > (rd2 ** 2) + variance:
+        b = numpy.log((improvement ** 2) - (rd2 ** 2) - variance)
+    else:
+        k = 1
+
+        while f(a - k * SYSTEM_CONSTANT, variance, improvement, rd2, a) < 0:
+            k += 1
+
+        b = a - k * SYSTEM_CONSTANT
+
+    Fa: float = f(a, variance, improvement, rd2, a)
+    Fb: float = f(a, variance, improvement, rd2, a)
+    Fc: float
+
+    while math.fabs(b - a) > 0.000001:
+        c = a + (a - b) * Fa / (Fb - Fa)
+        Fc = f(c, variance, improvement, rd2, a)
+
+        if Fc * Fb <= 0:
+            a = b
+            Fa = Fb
+        else:
+            Fa /= 2
+
+        b = c
+        Fb = Fc
+
+    return math.exp(a / 2)
+
+def pre_rating_rd(rd2: float, new_volatility: float) -> float:
+    return math.sqrt((rd2 ** 2) +(new_volatility ** 2))
+
+def new_rd(pr_rd2: float, variance: float) -> float:
+    return 1 / math.sqrt((1 / (pr_rd2 ** 2)) + (1 / variance))
+
+def new_glicko(new_rd2: float, glicko2: float, enemy_glicko2: float, enemy_rd2: float, result: float) -> float:
+    return glicko2 + (new_rd2 ** 2) * (g(enemy_rd2) * (result - E(glicko2, enemy_glicko2, enemy_rd2)))
